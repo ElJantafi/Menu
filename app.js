300c2f11069fff319cff1039b26b6e8d397c1a77
ElJantafi <eljantafisalaheddin@gmail.com>
2023-02-10 10:07:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,114 @@
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+      id: 10,
+      title: "bison steak",
+      category: "dinner",
+      price: 22.99,
+      img: "./images/item-10.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+];
+const menuSection = document.querySelector('.menu-section') ;
+const buttons = document.querySelectorAll('.btn-container button') ;
+
+buttons.forEach( butt => {
+      butt.addEventListener('click',() => {
+        document.getElementsByClassName('menu-section')[0].innerHTML = '' ;
+        if(butt.id == 'all'){
+             menu.forEach((item) => {display(item.title,item.price,item.img,item.desc) ;})
+        }else{
+          menu.forEach((item) => {
+            if(item.category == butt.id){
+              display(item.title,item.price,item.img,item.desc) ;
+            }});
+        }         
+})
+});
+
+function display(title,price,image,desc){
+     var div = document.createElement('div') ;
+     div.classList.add('item') ;
+     div.innerHTML = `<img src="${image}" alt="item">
+                       <div class="info"> 
+                       <header>
+                           <h3 class="title">${title}</h3>
+                           <h3 class="price">${'$' + price}</h3>
+                        </header>
+                           <p class="text">${desc}</p>
+                       </div>
+`;
+  document.getElementsByClassName('menu-section')[0].appendChild(div) ;
+};
+buttons[0].click() ;
